4ed3cfbb6db5b578084ca6a9a610487d2cb96114
LucaDoro <luca.doro31@gmail.com>
2025-09-25 15:20:15 +0200

    Updating

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-window.addEventListener("scroll", function () {
-  const navbar = document.getElementById("navbar");
-  if (window.scrollY > 10) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
+
